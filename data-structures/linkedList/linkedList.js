02b784b878166324ac2fe9c607af2a38cca2620f
anikjoyy <anikjoy.cse@gmail.com>
2023-09-19 01:53:45 +0600

    linkedlist push element

--- data-structures/linkedList/linkedList.js
+++ data-structures/linkedList/linkedList.js
@@ -12,6 +12,30 @@ class LinkedList {
     this.tail = this.head;
     this.length = 1;
   }
+
+  push(value) {
+    const newNode = new Node(value); //creating new node
+    //we need head and tail points to null, if(this.head){} means head is pointing something, so we write the opposite condition of it
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      /*
+        tail:{
+            value:4, 
+            next:{
+                value:7,
+                next: null
+            }
+        }
+        */
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this; //means we return all
+  }
 }
 
 let myLinkedList = new LinkedList(4);
+myLinkedList.push(7);

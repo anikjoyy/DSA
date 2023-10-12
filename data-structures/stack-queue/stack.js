aec8d48be5975d60f2381f39bab444f020e8dac1
anikjoyy <anikjoy.cse@gmail.com>
2023-10-12 15:05:39 +0600

    created stack push

--- data-structures/stack-queue/stack.js
+++ data-structures/stack-queue/stack.js
@@ -11,7 +11,22 @@ class Stack {
     this.top = newNode;
     this.length = 1;
   }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.top = newNode;
+    } else {
+      newNode.next = this.top;
+      this.top = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
-let myStack = new Stack(11);
+let myStack = new Stack(7);
+myStack.push(23);
+myStack.push(3);
+myStack.push(11);
 console.log(myStack);
